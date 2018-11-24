6b0a1dbd9db1327ce3321f27631e2b62e3f9d08a
Austin Sharman <asharman22@gmail.com>
2018-11-24 13:45:16 -0500

    Link to firebase, materialize, JQuery, and local javascript and css

---
+++ assets/javascript/logic.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyD8NvLROiMcBmHGv5tVxKjOBCm3ISO40HU",
+    authDomain: "studyhall-169e6.firebaseapp.com",
+    databaseURL: "https://studyhall-169e6.firebaseio.com",
+    projectId: "studyhall-169e6",
+    storageBucket: "studyhall-169e6.appspot.com",
+    messagingSenderId: "448862351238"
+  };
+  firebase.initializeApp(config);
+
+  database = firebase.database();
