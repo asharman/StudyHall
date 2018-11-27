47259ac8acda979f5d90fb0373dedadcbc07f87b
Austin Sharman <asharman22@gmail.com>
2018-11-27 09:43:42 -0500

    Push search results to database on click

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
-  $('.sidenav').sidenav();
+  $('.modal').modal();
 
 
   // Initialize Firebase
-  var config = {
+  let config = {
     apiKey: "AIzaSyD8NvLROiMcBmHGv5tVxKjOBCm3ISO40HU",
     authDomain: "studyhall-169e6.firebaseapp.com",
     databaseURL: "https://studyhall-169e6.firebaseio.com",
@@ -12,7 +12,7 @@ $(document).ready(function () {
     messagingSenderId: "448862351238"
   };
   firebase.initializeApp(config);
-  var uiConfig = {
+  let uiConfig = {
     signInSuccessUrl: '<url-to-redirect-to-on-success>',
     signInOptions: [
       // Leave the lines as is for the providers you want to offer your users.
@@ -29,92 +29,102 @@ $(document).ready(function () {
   };
 
   // Initialize the FirebaseUI Widget using Firebase.
-  var ui = new firebaseui.auth.AuthUI(firebase.auth());
+  let ui = new firebaseui.auth.AuthUI(firebase.auth());
   // The start method will wait until the DOM is loaded.
   ui.start('#firebaseui-auth-container', uiConfig);
 
-  var database = firebase.database();
+  let database = firebase.database();
 
-  var storage = firebase.storage();
-  var storageRef = storage.ref();
+  let storage = firebase.storage();
+  let storageRef = storage.ref();
 
-  var provider = new firebase.auth.GoogleAuthProvider();
+  let provider = new firebase.auth.GoogleAuthProvider();
 
   firebase.auth().getRedirectResult().then(function (result) {
     if (result.credential) {
       // This gives you a Google Access Token. You can use it to access the Google API.
-      var token = result.credential.accessToken;
+      let token = result.credential.accessToken;
       // ...
     }
     // The signed-in user info.
-    var user = result.user;
+    let user = result.user;
   }).catch(function (error) {
     // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
+    let errorCode = error.code;
+    let errorMessage = error.message;
     // The email of the user's account used.
-    var email = error.email;
+    let email = error.email;
     // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
+    let credential = error.credential;
     // ...
   });
   // $(".card").on("click", function() {
 
   // })
 
-  document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
-  });
-
-  // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
-  // var collapsibleElem = document.querySelector('.collapsible');
-  // var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
+  // Initialize collapsible (uncomment the lines below if you use the dropdown letiation)
+  // let collapsibleElem = document.querySelector('.collapsible');
+  // let collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
 
   // Or with jQuery
 
 
-});
 
-let queryURL = `https://api.foursquare.com/v2/venues/search?client_id=MRGWSL0B0JOCS24FEY2DXNMTQSPVX32A2QQ2WGLGXKPJ4OBM&client_secret=A5TGKNJQUCFJFLVHRC1R1BXIHN35GZYKLFPZVV5W11PTHA5T&v=20180323&near=Orlando, FL&query=coffee`;
+  let queryURL = `https://api.foursquare.com/v2/venues/search?client_id=MRGWSL0B0JOCS24FEY2DXNMTQSPVX32A2QQ2WGLGXKPJ4OBM&client_secret=A5TGKNJQUCFJFLVHRC1R1BXIHN35GZYKLFPZVV5W11PTHA5T&v=20180323&near=Orlando, FL&query=coffee`;
+  let object;
 
-console.log(`Working`);
+  console.log(`Working`);
 
-$("#searchButton").on("click", function () {
-  $("#card-container").empty();
-  $.ajax({
-    url: queryURL,
-    datatype: "json",
-    method: "GET",
-  }).then(function (response) {
-    console.log(response);
-    for (i in response.response.venues) {
-      let venueID = response.response.venues[i].id;
-      let name = response.response.venues[i].name;
-      let address = response.response.venues[i].location.formattedAddress;
-      console.log(name);
-      console.log(address[0]);
+  $("#searchButton").on("click", function () {
+    $("#card-container").empty();
+    $.ajax({
+      url: queryURL,
+      datatype: "json",
+      method: "GET",
+    }).then(function (response) {
+      console.log(response);
+      object = response;
+      for (i in response.response.venues) {
+        let venueID = response.response.venues[i].id;
+        let name = response.response.venues[i].name;
+        let address = response.response.venues[i].location.formattedAddress;
+        console.log(name);
+        console.log(address[0]);
 
-      if (address.length === 3) {
+        if (address.length === 3) {
 
-        $("#card-container").append(`
+          $("#card-container").append(`
           <div class="card card-limited hoverable">
-              <div data="${i}" class="card-image modal-trigger" href="#modal">
-                  <img src="http://wptest.io/demo/wp-content/uploads/sites/2/2012/12/unicorn-wallpaper.jpg">
-                  <span id="place" class="card-title">${name}</span>
-              </div>
-              <div class="card-content paragraph">
-                  <div class="row">
-                      <div class="col s9">
-                          <p id="address" class="col s9">${address[0]}</p>
-                      </div>
-                      <div class="col s3">
-                          <a data="${i}" id="addButton" class="btn-small waves-effect waves-light red right bottom hoverable"><i class="material-icons">ADD</i></a>
-                      </div>
-                  </div>
-              </div>
+          <div data="${i}" class="card-image modal-trigger" href="#modal">
+          <img src="http://wptest.io/demo/wp-content/uploads/sites/2/2012/12/unicorn-wallpaper.jpg">
+          <span id="place" class="card-title">${name}</span>
+          </div>
+          <div class="card-content paragraph">
+          <div class="row">
+          <div class="col s9">
+          <p id="address" class="col s9">${address[0]}</p>
+          </div>
+          <div class="col s3">
+          <a data="${i}" id="addButton" class="btn-small waves-effect waves-light red right bottom hoverable"><i class="material-icons">ADD</i></a>
+          </div>
+          </div>
+          </div>
           </div>`)
+        }
       }
-    }
+    });
   });
+
+  $("#card-container").on("click", "#addButton", function () {
+    let venueIndex = $(this).attr("data");
+    let venue = object.response.venues[venueIndex];
+    console.log(venue);
+
+    database.ref().push({
+      name: venue.name,
+      address: venue.location.formattedAddress[0],
+      ID: venue.id,
+    });
+  })
+
 });
